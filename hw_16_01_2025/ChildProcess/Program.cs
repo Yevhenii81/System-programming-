48e3650a5410a964b315fb25038e6301038d479a
Yevhenii81 <144814547+Yevhenii81@users.noreply.github.com>
2025-01-24 12:13:25 +0200

    Update Program.cs

--- hw_16_01_2025/ChildProcess/Program.cs
+++ hw_16_01_2025/ChildProcess/Program.cs
@@ -6,10 +6,9 @@ namespace ChildProcess
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("The child process works.");
+            Console.WriteLine("The child process works");
             Thread.Sleep(2000);
-            Console.WriteLine("Finish!");
-            //throw new Exception();
+            Console.WriteLine("Finish");
         }
     }
 }
