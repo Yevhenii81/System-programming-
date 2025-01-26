67c00aeb9d8a98f3bb826471531be8f8c0f88d57
Yevhenii81 <144814547+Yevhenii81@users.noreply.github.com>
2025-01-26 14:41:26 +0200

    Add files via upload

---
+++ hw_16_01_2025 (2)/ChildProcess/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ChildProcess
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The child process works.");
+            Thread.Sleep(3000);
+            Console.WriteLine("Finish!");
+        }
+    }
+}
