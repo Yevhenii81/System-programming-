d7cbf7cbae58e283661abcc4bb0f6958bdc2de54
Yevhenii81 <144814547+Yevhenii81@users.noreply.github.com>
2025-02-17 13:54:37 +0200

    Add files via upload

---
+++ hw_30_01_2025/hw_30_01_2025/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace hw_30_01_2025
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
